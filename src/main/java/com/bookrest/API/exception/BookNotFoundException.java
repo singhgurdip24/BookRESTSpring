e9a1e3637daaf6b7d23e965756c71dc909cc1bac
Gurdip Singh <gurdip.singh@sgdigital.com>
2019-06-18 00:31:59 -0400

    MVC defined

---
+++ src/main/java/com/bookrest/API/exception/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bookrest.API.exception;
+
+public class BookNotFoundException extends Exception {
+    private long book_id;
+    public BookNotFoundException(long book_id) {
+        super(String.format("Book is not found with id : '%s'", book_id));
+    }
+}
